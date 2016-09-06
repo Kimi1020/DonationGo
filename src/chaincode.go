e18bbb03ac3b32ee10474c8c2dac04d0c0c2eef1
Li Wei <lwsh@cn.ibm.com>
2016-09-06 20:43:26 +0800

    init request

--- src/chaincode.go
+++ src/chaincode.go
@@ -278,10 +278,12 @@ func (t *SimpleChaincode) read(stub *shim.ChaincodeStub, args []string) ([]byte,
 
     key = args[0]
     valAsbytes, err := stub.GetState(key)
+    var r Request
+    err = json.Unmarshal(valAsbytes, &r)
     if err != nil {
         jsonResp = "{\"Error\":\"Failed to get state for " + key + "\"}"
         return nil, errors.New(jsonResp)
     }
 
-    return valAsbytes, nil
+    return r.description, nil
 }
