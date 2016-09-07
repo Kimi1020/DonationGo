462b1628d1cf1f38527f87d6e5b892f21398f8a4
Li Wei <lwsh@cn.ibm.com>
2016-09-07 20:23:49 +0800

    v2

--- src/v2/chaincode.go
+++ src/v2/chaincode.go
@@ -122,7 +122,7 @@ func (t *SimpleChaincode) createDonation(stub *shim.ChaincodeStub, args []string
      if err != nil {
         return nil, err
      }
-     
+     fmt.Println(djson)
      stub.PutState("donationid", []byte(donation.Id))
      
      
