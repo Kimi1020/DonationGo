70c9f5c04678275a0d3bbef34f93ac6fe805168d
Li Wei <lwsh@cn.ibm.com>
2016-09-08 11:42:41 +0800

    version 4

--- src/v3/chaincode.go
+++ src/v3/chaincode.go
@@ -178,7 +178,7 @@ func (t *SimpleChaincode) createDonation(stub *shim.ChaincodeStub, args []string
     // }
     // donationList = append(donationList, donation.Id)
     // request.DonationList = donationList
-    return nil, nil     
+    
 }
 
 func (t *SimpleChaincode) Query(stub *shim.ChaincodeStub, function string, args []string) ([]byte, error) {
